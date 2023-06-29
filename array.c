4c9ad846ab232b7fc9fe7c8180a6c5d069792738
Mark-H1301 <138100634+Mark-H1301@users.noreply.github.com>
2023-06-29 15:49:58 +0100

    Create array.c
    main and random number function

---
+++ array.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define IM1 2147483563
+#define IM2 2147483399
+#define AM (1.0/IM1)
+#define IMM1 (IM1-1)
+#define IA1 40014
+#define IA2 40692
+#define IQ1 53668
+#define IQ2 52774
+#define IR1 12211
+#define IR2 3791
+#define NTAB 32
+#define NDIV (1+IMM1/NTAB)
+#define EPS 1.2e-7
+#define RNMX (1.0-EPS)
+
+float ran2(long *idum)
+{
+    int j;
+    long k;
+    static long idum2 = 123456789;
+    static long iy = 0;
+    static long iv[NTAB];
+    float temp;
+    if (*idum <= 0) //changed< to >
+    {
+        if (-(*idum) < 1)*idum = 1;
+        else *idum = -(*idum);
+        idum2 = (*idum);
+        for (j = NTAB + 7; j >= 0; j--)
+        {
+            k = (*idum) / IQ1;
+            *idum = IA1 * (*idum - k * IQ1) - k * IR1;
+            if (*idum < 0)*idum += IM1;
+            if (j < NTAB)iv[j] = *idum;//changed < to >
+        }
+        iy = iv[0];
+    }
+    k = (*idum) / IQ1;//chng k to j
+    *idum = IA1 * (*idum - k * IQ1) - k * IR1;
+    if (*idum < 0)*idum += IM1;
+    j = idum2 / IQ2;
+    *idum = IA2 * (*idum - k * IQ2) - k * IR2;
+    if (idum2 < 0)idum2 += IM2;
+    j = iy / NDIV;
+    iy = iv[j] - idum2;
+    iv[j] = *idum;
+    if (iy < 1)iy += IMM1;
+    if ((temp = AM * iy) > RNMX) return RNMX;
+    else return temp;
+}
+
+int main()
+{
+	long unsorted_array [100] = {};
+	float ans_array[100];
+	long num;
+	for(int i =0; i<100;i++)
+	{
+		ans_array[i] = ran2(&unsorted_array[i]);
+		printf("%f\n", ans_array[i]);
+	}
+	int notsort = 0;
+	long array_size = sizeof(unsorted_array)/sizeof(long);
+	float temp  = 0.0f;
+	float precision = 0.000001f;
+	do{
+		notsort = 0;
+		for(int i =0; i<array_size-1;i++)
+		{
+			if (ans_array[i] > ans_array[i+1])
+			{
+			temp = ans_array[i];
+			ans_array[i] = ans_array[i+1];
+			ans_array[i+1] = temp;
+			notsort = 1;
+			}
+		}
+	}while(notsort ==1);
+	printf("\n");
+	
+	for(int i =0; i<10;i++)
+	{
+		printf("%f\n", ans_array[i]);
+		
+	}
+}
